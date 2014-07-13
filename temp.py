c03c40120c5755f1b18fffcd2bf1e2f55548997a
Matthew Brown <me@matthewbrown.io>
2014-07-14 01:46:06 +1000

    Back to real sensor

--- temp.py
+++ temp.py
@@ -2,6 +2,23 @@ from multiprocessing import Process
 from setproctitle import setproctitle
 from random import randint
 import time
+import os
+import glob
+
+
+def read_temp_raw(device_file):
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp(device_file):
+    lines = read_temp_raw(device_file)
+    temperaturedata = lines[1].split(" ")[9]
+    temp_string = float(temperaturedata[2:])
+    temp_c = float(temp_string) / 1000.0
+    return temp_c
 
 
 class TemperatureReader():
@@ -23,7 +40,13 @@ class TemperatureReader():
 
     def startProcess(self, channel, queue):
         setproctitle("[M2M] Temperature Reader")
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
+
+        base_dir = '/sys/bus/w1/devices/'
+        device_folder = glob.glob(base_dir + '28*')[0]
+        device_file = device_folder + '/w1_slave'
         while True:
-            temp = randint(10, 15)
-            queue.put((channel, temp))
+            c = read_temp(device_file)
+            queue.put((channel, c))
             time.sleep(5)
